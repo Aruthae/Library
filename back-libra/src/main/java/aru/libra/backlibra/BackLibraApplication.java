0db2fe571213d6ea92015fd83f7e1a104401281f
Aruthae <laudoux.gilles@laposte.net>
2023-07-31 18:47:29 +0200

    le lancement du projet

---
+++ back-libra/src/main/java/aru/libra/backlibra/BackLibraApplication.java
@@ -0,0 +1,13 @@
+package aru.libra.backlibra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackLibraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackLibraApplication.class, args);
+	}
+
+}
---
+++ back-libra/src/test/java/aru/libra/backlibra/BackLibraApplicationTests.java
@@ -0,0 +1,13 @@
+package aru.libra.backlibra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackLibraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
